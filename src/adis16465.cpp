8a1b04a1b0f26b8c3f0852964896f0077657ab27
uhobeike <beike315@icloud.com>
2023-04-15 18:44:00 +0900

    Remove ROS

--- src/adis16465.cpp
+++ src/adis16465.cpp
@@ -30,7 +30,6 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 // OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// #include "ros/ros.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
